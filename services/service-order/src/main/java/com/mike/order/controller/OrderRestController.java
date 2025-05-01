af962aa8100c94a8567a635fd5c1f4283d42098a
ck237 <ck237@hotmail.com>
2025-05-01 20:47:53 +0800

    NEW: 完成負載平衡

--- services/service-order/src/main/java/com/mike/order/controller/OrderRestController.java
+++ services/service-order/src/main/java/com/mike/order/controller/OrderRestController.java
@@ -1,13 +1,11 @@
 package com.mike.order.controller;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import order.OrderDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -19,42 +17,37 @@ import product.ProductDto;
 @RestController
 public class OrderRestController {
 
-    private final RestTemplate restTemplate = new RestTemplate();
-
     @Autowired
-    private DiscoveryClient discoveryClient;
+    RestTemplate restTemplate;
 
     @GetMapping("/getOrder")
-    public OrderDto getOrder(){
-        OrderDto orderDto =  new OrderDto();
+    public OrderDto getOrder() {
+        OrderDto orderDto = new OrderDto();
         orderDto.setId(0L);
         orderDto.setTotalAmount(new BigDecimal("0"));
         orderDto.setUserId(0L);
         orderDto.setNickName("");
         orderDto.setAddress("");
         // 這個方法
-        List<ProductDto>productDtos = getProductDtoList();
+        List<ProductDto> productDtos = getProductDtoList();
         orderDto.setProductDtoList(productDtos);
         return orderDto;
     }
 
-    private List<ProductDto> getProductDtoList(){
+    private List<ProductDto> getProductDtoList() {
         List<ProductDto> productDtoList = new ArrayList<>();
-        List<ServiceInstance>instances = discoveryClient.getInstances("seata-product");
-        if (!instances.isEmpty()) {
-            ServiceInstance instance = instances.get(0);
-            String url = "http://" + "127.0.0.1" + ":" + instance.getPort() + "/findProduct";
-            System.out.println(url);
 
-            List<ProductDto> a = restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    HttpEntity.EMPTY,
-                    new ParameterizedTypeReference<List<ProductDto>>() {}
-            ).getBody();
-            System.out.println(a);
-        }
-        return  productDtoList;
+        String url = "http://seata-product/findProduct";
+
+        productDtoList = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                new ParameterizedTypeReference<List<ProductDto>>() {
+                }
+        ).getBody();
+
+        return productDtoList;
     }
 
 }
