b35471b4a690865c1b66b8256cf9f900516f16eb
ck237 <ck237@hotmail.com>
2025-05-01 19:38:19 +0800

    deBug: 找到打List的寫法

--- services/service-order/src/main/java/com/mike/order/controller/OrderRestController.java
+++ services/service-order/src/main/java/com/mike/order/controller/OrderRestController.java
@@ -8,13 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import product.ProductDto;
 
 @RestController
 public class OrderRestController {
 
+    private final RestTemplate restTemplate = new RestTemplate();
 
     @Autowired
     private DiscoveryClient discoveryClient;
@@ -38,8 +43,16 @@ public class OrderRestController {
         List<ServiceInstance>instances = discoveryClient.getInstances("seata-product");
         if (!instances.isEmpty()) {
             ServiceInstance instance = instances.get(0);
-            String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/getProduct";
+            String url = "http://" + "127.0.0.1" + ":" + instance.getPort() + "/findProduct";
             System.out.println(url);
+
+            List<ProductDto> a = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    HttpEntity.EMPTY,
+                    new ParameterizedTypeReference<List<ProductDto>>() {}
+            ).getBody();
+            System.out.println(a);
         }
         return  productDtoList;
     }
