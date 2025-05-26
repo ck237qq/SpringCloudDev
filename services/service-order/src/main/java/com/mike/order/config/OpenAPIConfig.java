8ba46371d1b5240e64bab47f96f17422a6913a97
ChenMike <mikepg0401@gmail.com>
2025-05-26 16:47:48 +0800

    Bug: Swagger可以運作但API路徑設定不太對

---
+++ services/service-order/src/main/java/com/mike/order/config/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package com.mike.order.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("http://localhost:10000")))
+                .info(new Info()
+                        .title("訂單API")
+                        .description("開發測試中")
+                        .version("1.0.0"));
+    }
+}
